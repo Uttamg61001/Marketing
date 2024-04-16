57619c6d71cd49acda3ae7f3d8aefceba3d8f1be
Uttamg61001 <uttamg61001.com>
2024-04-16 23:57:41 +0530

    card selection update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
       {/* <HeroSelection /> */}
-      <CardSelection />
+      {/* <CardSelection /> */}
       <ServiceCard />
       <Content />
       <Form />
